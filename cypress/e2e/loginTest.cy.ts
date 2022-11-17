b4b6e8ba8b078315646ad7e1eb96cba4f23fc52a
Ahmed Rebai <ahmed.bouhmid94@gmail.com>
2022-11-17 18:12:24 +0100

    add cypress env variables

--- cypress/e2e/loginTest.cy.ts
+++ cypress/e2e/loginTest.cy.ts
@@ -1,7 +1,7 @@
 describe('Login test', () => {
 
   it('Should sign in and redirected to home page', () => {
-    cy.visit('http://localhost:4200/')
+    cy.visit(Cypress.env('appURL'))
     cy.get('[data-cy=login-button]').click()
     .then(()=>{
       cy.url().then(url=>{
@@ -9,14 +9,12 @@ describe('Login test', () => {
 
      })
     })
-
-    cy.get('[data-cy=login-email-input]').type("test@mail.com")
-    cy.get('[data-cy=login-password-input]').type("testcypress")
+    cy.get('[data-cy=login-email-input]').type(Cypress.env('userEmail'))
+    cy.get('[data-cy=login-password-input]').type(Cypress.env('userPassword'))
     cy.get('[data-cy=login-button]').click({multiple:true});
     cy.get('[data-cy=logout-link]').should('exist')
     cy.url().then(url=>{
       cy.url().should('contain', 'home');
-
    })
   })
 })
