7fbfc37de48ee8aebcec4c6b40b38d35b5dea84a
Ahmed Rebai <ahmed.bouhmid94@gmail.com>
2022-11-20 17:49:09 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyABgdv3E4flqXkC-sFD0w6XLCVXO35nzkQ",
-    authDomain: "ng2-firebase-ce077.firebaseapp.com",
-    databaseURL: "https://ng2-firebase-ce077.firebaseio.com",
-    projectId: "ng2-firebase-ce077",
-    storageBucket: "ng2-firebase-ce077.appspot.com",
-    messagingSenderId: "627200279654",
-    appId: "1:627200279654:web:ff0409eff5b97df2c299a7"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    databaseURL: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 };
 
